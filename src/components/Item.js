7f78003745828e8639824b76faa47c110c5708a8
Brian Ngetich <kipronobrian97@gmail.com>
2024-05-31 15:13:16 +0300

    Using Hooks

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart,setCart]= useState(false)
+
+  function handleCart (){
+    setCart((inCart) => !inCart)
+  }
+
+
+  const buttonText = inCart ?  "Remove from Cart" : "Add to Cart"; // conditional button text
+
+  
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? "remove" : "add"} onClick={handleCart}>{buttonText}</button>
     </li>
   );
 }
